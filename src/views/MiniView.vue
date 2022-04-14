15e649f7c7ec54e37274659d3aa52beeac8ec50f
Daniel Källstrand Modig <daniel@ikoncept.se>
2022-04-14 20:17:58 +0200

    Fixes

--- src/views/MiniView.vue
+++ src/views/MiniView.vue
@@ -1,6 +1,9 @@
 <template>
-    <div class="flex w-screen h-screen pt-20">
-        <div class="relative w-4/5 p-8 mx-auto mb-auto bg-gray-800 bg-opacity-[.99] font-fira">
+    <div class="flex w-screen h-screen pt-20 bg-gray-300 bg-opacity-30">
+        <div
+            class="relative w-4/5 p-8 mx-auto mb-auto bg-gray-900 bg-opacity-[.99] font-fira drop-shadow-3xl rounded"
+            @click.self="sendMessage({type: 'close-dialog'})"
+        >
             <BookmarksRow />
         </div>
     </div>
@@ -11,13 +14,24 @@ import BookmarksRow from '../components/BookmarksRow.vue'
 
 export default {
     components: {
-
         BookmarksRow
-
     },
 
     mounted(){
         
+    },
+
+    methods: {
+        sendMessage(payload){
+            const params = { 
+                active: true,
+                currentWindow: true 
+            }
+
+            chrome.tabs.query(params, (tabs) => {
+                chrome.tabs.sendMessage(tabs[0].id, payload)
+            })
+        }
     }
 }
 </script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
         extend: {
             fontFamily: {
                 'fira': ['Fira Code', 'mono'] 
+            },
+            dropShadow: {
+                '3xl': '0 35px 35px rgba(0, 0, 0, 0.5)'
             }
         }
     },
