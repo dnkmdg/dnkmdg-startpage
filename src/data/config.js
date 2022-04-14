d7ca6849e714ebf2826b74fdb0d17c115fc37ea1
Daniel Källstrand Modig <daniel@ikoncept.se>
2022-04-14 14:16:22 +0200

    keytrap mostly done

---
+++ src/data/config.js
@@ -0,0 +1,13 @@
+export const keys = [
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 
+    'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 
+    'S', 'T', 'U', 'W', 'V', 'X', 'Y', 'Z',
+
+    'AA', 'AB', 'AC', 'AD', 'AE', 'AF', 'AG', 'AH', 'AI', 
+    'AJ', 'AK', 'AL', 'AM', 'AN', 'AO', 'AP', 'AQ', 'AR', 
+    'AS', 'AT', 'AU', 'AW', 'AV', 'AX', 'AY', 'AZ',
+
+    'BA', 'BB', 'BC', 'BD', 'BE', 'BF', 'BG', 'BH', 'BI', 
+    'BJ', 'BK', 'BL', 'BM', 'BN', 'BO', 'BP', 'BQ', 'BR', 
+    'BS', 'BT', 'BU', 'BW', 'BV', 'BX', 'BY', 'BZ'
+]
